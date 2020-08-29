116488842c2ef46783004b11bf73668e08eecf5d
Indu1198 <40969997+madhuindu@users.noreply.github.com>
2020-08-29 13:03:59 +0530

    Create student.js
    student details

---
+++ student.js
@@ -0,0 +1,56 @@
+const students = [
+  {
+    id: 1,
+    firstName: "Arun",
+    lastName: "Kumar",
+    age: 20,
+    gender: "Male",
+    scores: {
+      math: 98,
+      english: 99,
+      science: 97,
+      tamil: 96
+    }
+  },
+  {
+    id: 2,
+    firstName: "Madhu",
+    lastName: "Mitha",
+    age: 21,
+    gender: "Female",
+    scores: {
+      math: 100,
+      english: 87,
+      science: 99,
+      tamil: 67
+    }
+  },
+  {
+    id: 3,
+    firstName: "Vijay",
+    lastName: "Kumar",
+    age: 21,
+    gender: "Male",
+    scores: {
+      math: 90,
+      english: 77,
+      science: 90,
+      tamil: 97
+    }
+  },
+  {
+    id: 4,
+    firstName: "Indu",
+    lastName: "Mathi",
+    age: 21,
+    gender: "Female",
+    scores: {
+      math: 80,
+      english: 88,
+      science: 93,
+      tamil: 69
+    }
+  }
+];
+
+module.exports = students;
